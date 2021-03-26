c16de4ee53723856ff382f765e9c43fc898c7129
Andres Herrera <andres.2050@hotmail.com>
2021-03-26 11:09:51 -0500

    Add Search field stiling
    Add material package and apply style to search field

--- WeatherClient/ClientApp/src/components/CityInput.tsx
+++ WeatherClient/ClientApp/src/components/CityInput.tsx
@@ -1,28 +1,59 @@
-﻿import React, {ChangeEvent} from "react";
+﻿import React from "react";
+import {IconButton, InputBase, Paper} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import {Search} from '@material-ui/icons';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        padding: '2px 4px',
+        display: 'flex',
+        alignItems: 'center',
+        width: 400,
+    },
+    input: {
+        marginLeft: theme.spacing(1),
+        flex: 1,
+    },
+    iconButton: {
+        padding: 10,
+    },
+}));
 
 interface CityInputProps {
     searchCity(city: string): void;
 }
 
-export const CityInput:React.FC<CityInputProps> = ({ searchCity }) => {
-    const [city, setCity] = React.useState("")
-    
-    const updateCity = (event: ChangeEvent<HTMLInputElement>) => {
+export const CityInput: React.FC<CityInputProps> = ({searchCity}) => {
+    const [city, setCity] = React.useState("");
+    const classes = useStyles();
+
+    const updateCity = (event: React.ChangeEvent<HTMLInputElement>) => {
         setCity(event.target.value)
     }
-    
-    const onSearchCityClick = () => {
-        searchCity(city.trim())
-        setCity("")
+
+    const onSearchCityClick = (event: React.FormEvent) => {
+        event.preventDefault();
+        const search = city.trim().toLowerCase();
+        if (search === "") {
+            return;
+        }
+
+        searchCity(search);
+        setCity("");
     }
-    
+
     return (
-        <div>
-            <input type="text" name="city"
-                   value={city}
-                   onChange={updateCity}
-                   placeholder="Buscar ciudad" />
-            <button onClick={onSearchCityClick}>Buscar</button>
-        </div>
+        <Paper component="form" onSubmit={onSearchCityClick} className={classes.root}>
+            <InputBase
+                className={classes.input}
+                placeholder="Buscar ciudad"
+                value={city}
+                onChange={updateCity}
+                inputProps={{ 'aria-label': 'Devuelve la informacion climatica en una ciudad' }}
+            />
+            <IconButton type="submit" className={classes.iconButton} aria-label="search">
+                <Search />
+            </IconButton>
+        </Paper>
     );
 }
